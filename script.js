64278c10d577111bad1d1f77ec7a4fc737d9435d
Carter Pool <83323282+hhhhhhhh75@users.noreply.github.com>
2024-02-23 12:15:32 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,90 @@
+function toggleMenuOn(){
+	document.getElementById("side-menu").style.display="flex";
+	document.getElementById("main-menu").style.display="none";
+	document.getElementById("logo").style.display="none";
+	document.getElementById("main-content").style.display="none";
+	document.getElementById("nav").style.filter="invert(1)";
+}
+
+function toggleMenuOff(){
+	document.getElementById("side-menu").style.display="none";
+	document.getElementById("main-menu").style.display="flex";
+	document.getElementById("logo").style.display="flex";
+	document.getElementById("main-content").style.display="flex";
+	document.getElementById("nav").style.filter="invert(0)";
+}
+
+function lightboxMainImage(img){
+	var name = img.src;
+	let source = document.getElementById("lightbox-image");
+	source.src=name;
+	document.getElementById("lightbox").style.display="flex";
+	document.getElementById("photos-container").style.display="none";
+	document.getElementById("lightbox-buttons").style.display="flex";
+}
+
+function closeLightbox(){
+	document.getElementById("lightbox-buttons").style.display="none";
+	document.getElementById("lightbox").style.display="none";
+	document.getElementById("photos-container").style.display="flex";
+}
+
+function fadeIn(){
+	document.getElementById("transition-image").style.opacity = "1";
+	document.getElementById("transition-image").style.transform = "scale(1)";
+
+}
+
+function loadMainPhotos(){ 
+	const listItems = document.querySelectorAll('#transition-image');
+	let len = listItems.length;
+	for (let i = 1; i < listItems.length; i++){
+		listItems[i].setAttribute("id", "transition-image");
+		listItems[i - 1].setAttribute("id", "");
+	
+	}
+	
+	
+}
+
+
+
+function nextImage(val) {
+	const listItems = document.querySelectorAll('img');
+	let len = listItems.length;
+	const listImages = [];
+	let currentImage = document.getElementById("lightbox-image").src;
+	for (let i = 0; i < len; i++){
+		listImages.push(listItems[i].src);
+	}
+
+	
+	
+	if (document.getElementById("lightbox-image").classList.contains("fadeIn")){
+		document.getElementById("lightbox-image").classList.remove("fadeIn");
+		document.getElementById("lightbox-image").classList.add("fadeOut");
+	}
+	else {
+
+		document.getElementById("lightbox-image").classList.remove("fadeOut");
+		document.getElementById("lightbox-image").classList.add("fadeIn");
+		}
+	
+	for (let i = 0; i < listImages.length -1; i++){
+		
+		if (listImages[i] === currentImage){
+			if (i===0 && val===-1) {
+				i = i;
+			}
+			else {
+				document.getElementById("lightbox-image").src=listImages[i+val];
+			}
+		
+		}
+	}
+	
+	
+	
+	
+}
+
